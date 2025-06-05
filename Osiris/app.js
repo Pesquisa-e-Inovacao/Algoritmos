669308101f93a3af4497e037a39092e6049c1d8f
Matheus <matheus.csoares@sptech.school>
2025-06-04 21:40:58 -0300

    bobia conectado com a API web-data-viz, junto de validação no login

--- Osiris/app.js
+++ Osiris/app.js
@@ -1,28 +1,86 @@
-// var ambiente_processo = 'producao';
-var ambiente_processo = 'desenvolvimento';
 
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
-// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
-// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
-
-require("dotenv").config({ path: caminho_env });
-
 var express = require("express");
-var cors = require("cors");
-var path = require("path");
-var PORTA_APP = process.env.APP_PORT;
-var HOST_APP = process.env.APP_HOST;
-
 var app = express();
 
-var indexRouter = require("./src/routes/index");
-var usuarioRouter = require("./src/routes/usuarios");
-var dashRouter = require("./src/routes/dash");
+var cors = require("cors");
+var path = require("path");
+require("dotenv").config({ path: caminho_env });
+const PORTA_SERVIDOR = process.env.PORTA;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
+
+const { GoogleGenAI } = require("@google/genai");
+
+
+
+
+
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
+// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+async function gerarResposta(mensagem) {
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `Responda apenas perguntas relacionadas a vazamento de gás em silos de soja. Isso inclui temas como:
+
+Presença e riscos de gases (ex.: metanol, CO₂, etc.)
+
+Soluções ou medidas para lidar com vazamentos
+
+Formas de prevenir ou postergar problemas relacionados a gases
+
+Se a pergunta não estiver relacionada a silos de soja ou aos tópicos acima, responda apenas com:
+"Não posso responder isso.". Pergunta: ${mensagem}`
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+var PORTA_APP = process.env.APP_PORT;
+var HOST_APP = process.env.APP_HOST;
+
+var indexRouter = require("./src/routes/index");
+var usuarioRouter = require("./src/routes/usuarios");
+var dashRouter = require("./src/routes/dash");
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
@@ -43,4 +101,19 @@ app.listen(PORTA_APP, function () {
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
+});
+
+app.listen(PORTA_SERVIDOR, () => {
+    console.info(
+        `
+        ######                ###    #    
+        #     #  ####  #####   #    # #   
+        #     # #    # #    #  #   #   #  
+        ######  #    # #####   #  #     # 
+        #     # #    # #    #  #  ####### 
+        #     # #    # #    #  #  #     # 
+        ######   ####  #####  ### #     # 
+        `
+    );
+    console.info(`A API BobIA iniciada, acesse http://localhost:${PORTA_SERVIDOR}`);
 });
